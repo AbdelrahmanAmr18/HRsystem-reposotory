2a878f23adae9cf9988bcf4cf2fba2b51b3567fc
AbdelrahmanAmr18 <abdelrahmanamr1812@gmail.com>
2023-11-19 16:57:34 +0200

    HR-System first commit
    Initial Migration done and database created

---
+++ Backend/HR-System/HR-System-Domain/Entities/Branch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_System_Domain.Entities
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string CurrentAddress { get; set; } = string.Empty;
+        public string PerminentAddress { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Website { get; set; } = string.Empty;
+        public string Fax { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        [ForeignKey("Company")]
+        public int? CompanyId { get; set; }
+        public virtual Company? Company { get; set; }
+
+        public virtual ICollection<Employee>? Employees { get; set; }
+        = new HashSet<Employee>();
+
+        //public int DepartmentId { get; set; }
+
+        //[ForeignKey("DepartmentId")]
+        //public virtual Branch? Department { get; set; }
+
+        //public int EmployeeId { get; set; }
+
+        //[ForeignKey("EmployeeId")]
+        //public virtual Employee? Employee { get; set; }
+
+    }
+}
---
+++ Backend/HR-System/HR-System-Domain/Entities/Level.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_System_Domain.Entities
+{
+    public class Level
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public virtual ICollection<Employee>? Employees { get; set; }
+        //public int ManagerId { get; set; }
+        //public virtual Employee? Manager { get; set; }
+
+        //public int EmployeeId { get; set; }
+        //public virtual Employee? Employee { get; set; }
+    }
+}
